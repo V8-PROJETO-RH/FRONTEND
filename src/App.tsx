faa666e8cf54b0a1337bfe883460fb93dc739668
Matheus Castro <castro.dev37@gmail.com>
2025-01-22 09:04:22 -0300

    feat: layout adm

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,18 @@ function App() {
         <Route path="/" element={<LayoutUser pageSelected='home' />}>
         
         </Route>
-        <Route path='/adm' element={<LayoutAdmin pageSelected='jobs' />} />
+
+        <Route path='/adm/jobs' element={<LayoutAdmin pageSelected='jobs' />} >
+
+        </Route>
+
+        <Route path='/adm/candidates' element={<LayoutAdmin pageSelected='candidates' />} >
+
+        </Route>
+
+        <Route path='/adm/dashboard' element={<LayoutAdmin pageSelected='dashboard' />} >
+
+        </Route>
       </Routes>
     </Router>
   );
