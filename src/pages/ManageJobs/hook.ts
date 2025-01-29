a5df5252f594bb85a447dc9c177252cfc5329cfb
Matheus Castro <castro.dev37@gmail.com>
2025-01-29 14:17:26 -0300

    wip

--- src/pages/ManageJobs/hook.ts
+++ src/pages/ManageJobs/hook.ts
@@ -4,17 +4,25 @@ import { JobManagement } from "../../types/job";
 import { useNavigate } from "react-router-dom";
 
 export default function useManageJobs() {
-  const { innerHeight } = window;
-  const heightViewport = innerHeight / 2;
-  console.log(heightViewport);
-
   const navigate = useNavigate();
 
   const [jobsList, setJobsList] = useState<JobManagement[]>();
 
   useEffect(() => {
+    const rootElement = document.documentElement;
+    const computedStyle = getComputedStyle(rootElement);
+    const fontSize = Number(computedStyle.fontSize.substring(0, 2));
+    const textSize = fontSize * 1.25;
+    const textPadding = (fontSize * 0.75) * 2;
+    const heigthTableCell = textPadding + textSize;
+
+    const { innerHeight } = window;
+    const height = innerHeight / 2;
+
+    const quantityElementsRequest = Math.floor(height / heigthTableCell - 1);
+
     async function setJobs() {
-      const jobsData = await getAllJobs();
+      const jobsData = await getAllJobs(0, quantityElementsRequest);
 
       if(jobsData) {
         const listJobs: JobManagement[] = jobsData.vagas.map(job => {
