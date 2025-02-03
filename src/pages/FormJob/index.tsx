e9553ea50448c6a8d460aa0168c7d13ffccedb68
Matheus Castro <castro.dev37@gmail.com>
2025-02-03 14:20:45 -0300

    feat: form styled

--- src/pages/FormJob/index.tsx
+++ src/pages/FormJob/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../../components/Button";
 import Checkbox from "../../components/Checkbox";
 import Input from "../../components/Input";
 
@@ -8,8 +9,8 @@ export default function FormJob() {
         <h1 className="text-2xl font-semibold">Criar vaga</h1>
       </div>
 
-      <div className="flex gap-12 px-10 pt-5">
-        <div className="w-1/3 flex flex-col gap-4">
+      <div className="flex gap-12 px-10 2xl:px-5 pt-5">
+        <div className="w-1/3 flex flex-col gap-4 2xl:gap-12">
           <Input className="[&>label]:mb-1" label="Nome:" placeholder="Digite o nome..." />
 
           <Input className="[&>label]:mb-1" label="Descrição:" placeholder="Digite a descrição..." />
@@ -19,7 +20,7 @@ export default function FormJob() {
           <Input className="[&>label]:mb-1" label="Requisitos:" placeholder="Digite os requisitos..." />
         </div>
 
-        <div className="w-1/3 flex flex-col gap-4">
+        <div className="w-1/3 flex flex-col gap-4 2xl:gap-12">
           <Input className="[&>label]:mb-1" label="Salario:" placeholder="Digite o salario..." />
 
           <Input className="[&>label]:mb-1" label="Beneficios:" placeholder="Digite os beneficios..." />
@@ -29,12 +30,12 @@ export default function FormJob() {
             <div className="flex gap-11 py-2">
               <div className="flex">
                 <Checkbox id="contract-clt" value="contract-clt" name="contracts" type="radio" />
-                <span>CLT</span>
+                <label htmlFor="contract-clt" className="cursor-pointer">CLT</label>
               </div>
 
               <div className="flex">
                 <Checkbox id="contract-pj" value="contract-pj" name="contracts" type="radio" />
-                <span>PJ</span>
+                <label htmlFor="contract-pj" className="cursor-pointer">PJ</label>
               </div>
             </div>
           </div>
@@ -44,21 +45,52 @@ export default function FormJob() {
             <div className="w-full flex gap-4">
               <div className="flex items-center">
                 <Checkbox id="model-hibrid" value="model-hibrid" name="models" type="radio" />
-                <label htmlFor="model-hibrid">Hibrido</label>
+                <label htmlFor="model-hibrid" className="cursor-pointer">Hibrido</label>
               </div>
 
               <div className="flex items-center">
                 <Checkbox id="model-on-site" value="model-on-site" name="models" type="radio" />
-                <label htmlFor="model-on-site">Presencial</label>
+                <label htmlFor="model-on-site" className="cursor-pointer">Presencial</label>
               </div>
 
               <div className="flex items-center">
                 <Checkbox id="model-home" value="model-home" name="models" type="radio" />
-                <label htmlFor="model-home">Home Office</label>
+                <label htmlFor="model-home" className="cursor-pointer">Home Office</label>
               </div>
             </div>
           </div>
         </div>
+
+        <div className="w-1/3 flex flex-col gap-4 2xl:gap-12">
+          <Input className="[&>label]:mb-1" label="Quantidade:" placeholder="Digite a quantidade..." />
+
+          <Input className="[&>label]:mb-1" label="Responsavel:" placeholder="Digite o responsavel..." />
+
+          <div>
+            <span className="font-semibold mb-2 flex">Status:</span>
+            <div className="w-full flex flex-col 2xl:flex-row gap-4">
+              <div className="flex items-center">
+                <Checkbox id="status-progress" value="status-progress" name="status" type="radio" />
+                <label htmlFor="status-progress" className="cursor-pointer">Andamento</label>
+              </div>
+
+              <div className="flex items-center">
+                <Checkbox id="status-freeze" value="status-freeze" name="status" type="radio" />
+                <label htmlFor="status-freeze" className="cursor-pointer">Congelada</label>
+              </div>
+
+              <div className="flex items-center">
+                <Checkbox id="status-completed" value="status-completed" name="status" type="radio" />
+                <label htmlFor="status-completed" className="cursor-pointer">Concluida</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center gap-7 my-10">
+        <Button variant="secondary" className="!bg-transparent-gray">Fechar</Button>
+        <Button variant="primary">Salvar</Button>
       </div>
     </div>
   );
